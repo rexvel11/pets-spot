55a1fa5346bcd8363df71bcb83d45fc6a9e24703
Rexcel Gigantone <rexzcel11@gmail.com>
2025-02-18 01:27:23 +0800

    slider finished

--- components/Home/Slider.jsx
+++ components/Home/Slider.jsx
@@ -1,15 +1,50 @@
-import { View, Text, FlatList, Image, StyleSheet } from 'react-native'
+import { View, Text, FlatList, Image, StyleSheet, Dimensions } from 'react-native'
 import React, { useEffect, useState } from 'react'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, doc, getDocs } from 'firebase/firestore'
 import {db} from './../../config/FirebaseConfig'
 import { use } from 'react'
 
 export default function Slider() {
 
+  const [sliderList,setSliderList]=useState([]);
+  useEffect(()=>{
+      GetSliders();
+  }, [])
+  const GetSliders=async()=>{
+    setSliderList([]);
+    const snapshot = await getDocs(collection(db, 'Sliders'));
+      snapshot.forEach((doc)=>{
+        console.log(doc.data());
+        setSliderList(sliderList=>[...sliderList,doc.data()])
+      })
+  }
+
   return (
-    <View>
-      <Text>Pogi mo rexcel</Text>
+    <View style={{
+      marginTop: 40,
+      height: 150
+    }}>
+      <FlatList 
+      horizontal={true}
+      showsHorizontalScrollIndicator={false}
+        data={sliderList}
+        renderItem={({item, index})=>(
+          <View>
+              <Image source={{uri:item?.imageUrl}}
+                style={styles?.sliderImage}
+              />
+          </View>
+        )}
+      />
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  sliderImage:{
+    width: Dimensions.get('screen').width*0.9,
+    height: 150,
+    borderRadius: 15,
+    marginRight: 15
+  }
+})
